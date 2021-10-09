57bd2e8a210046dca742ea7be719ba3a87fe0158
Veronika <veron9399bog@gmail.com>
2021-10-09 17:22:16 +0300

    feat: implement map

--- museum-adaptive/js/script.js
+++ museum-adaptive/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 console.log(
-`Score: 87 / 150`
+// `Score: 87 / 150`
 
 );
 
@@ -12,3 +12,46 @@ function myFunction() {
 		myTop.className += " responsive";
 	}
 }
+
+
+
+
+// Map in Contacts
+
+(function createMap() {
+const apiKey = 'pk.eyJ1IjoiYWxmcmVkMjAxNiIsImEiOiJja2RoMHkyd2wwdnZjMnJ0MTJwbnVmeng5In0.E4QbAFjiWLY8k3AFhDtErA';
+
+const myMap = L.map('map').setView([48.86091, 2.3364], 17);
+
+L.tileLayer(
+	'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+	{
+		maxZoom: 18,
+		id: 'mapbox/light-v10',
+		tileSize: 512,
+		zoomOffset: -1,
+		accessToken: apiKey,
+	},
+).addTo(myMap);
+
+var myIcon = L.icon({
+	iconUrl: 'assets/svg/place_black_24dp.svg',
+	iconSize: [35, 35],
+});
+
+const mainMarker = L.marker([48.86091, 2.3364], { icon: myIcon }).addTo(myMap);
+document.querySelectorAll('.leaflet-marker-icon').forEach(i => (i.style.opacity = 0.6));
+
+// Adding Marker
+let mapPopup = [
+	[48.8602, 2.3333],
+	[48.8607, 2.3397],
+	[48.8619, 2.333],
+	[48.8625, 2.3365]
+];
+mapPopup.forEach(point => {
+	const marker = L.marker(point, { icon: myIcon }).addTo(myMap);
+});
+document.querySelectorAll('.leaflet-marker-icon').forEach(i => (i == 0 ? i.style.opacity = 1 : i.style.opacity = 0.6));
+
+})();
