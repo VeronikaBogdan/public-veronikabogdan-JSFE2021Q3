6aa87b4b5d12339915d6b850631ad93d10a28f18
Veronika <veron9399bog@gmail.com>
2021-10-25 16:56:15 +0300

    feat: add greeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,25 +1,50 @@
 "use strict";
 
-// Time and calendar
-const timeT = document.querySelector(".time");
-const dateT = document.querySelector('.date');
+//! Time and calendar
+const timeT = document.querySelector(".time"),
+      dateT = document.querySelector('.date'),
+      greeting = document.querySelector('.greeting');
 
 function showTime() {
   const date = new Date();
   
   timeT.textContent = date.toLocaleTimeString();
   setTimeout(showTime, 1000);
+
   showDate();
+	showGreeting();
+
 }
 
 function showDate() {
   const date = new Date();
-  const options = {month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC'};
+  const options = {month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
  
-  dateT.textContent = date.toLocaleDateString('en-US', options);
+  dateT.textContent = date.toLocaleDateString('ru-RU', options);
   setTimeout(showDate, 1000);
 }
 
 
+//! GREETING
+function getHours() {
+  const date = new Date();
+  const hours = date.getHours();
+	return hours;
+}
+
+const getTimeOfDay = () => {
+  const timeOfDay = ['night', 'morning', 'day', 'evening'];
+	return timeOfDay[~~(getHours()/6)];
+}
+
+function showGreeting() {
+	// const timeOfDay = getTimeOfDay();
+	const greetingText = `Good ${getTimeOfDay()}`;
+
+	greeting.textContent = greetingText;
+}
+
 
 showTime();
+getHours();
+// getTimeOfDay();
