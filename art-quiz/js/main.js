3eb8e98b70ff212aa184e39129082ecc40c884e4
Veronika <veron9399bog@gmail.com>
2021-11-24 21:48:11 +0300

    feat: add settings page

--- art-quiz/js/main.js
+++ art-quiz/js/main.js
@@ -1,7 +1,12 @@
 const mainScreen = document.querySelector('.main-screen');
 const artists = document.querySelector('.artists');
 const pictures = document.querySelector('.pictures');
+
 const settings = document.querySelector('.settings');
+const settingsBtn = document.querySelector('.settings-btn');
+const saveBtn = document.querySelector('.save-btn');
+
+const categories = document.querySelector('.categories');
 
 
 artists.addEventListener('click', event => {
@@ -13,4 +18,12 @@ pictures.addEventListener('click', event => {
 	mainScreen.classList.add('hide');
 })
 
+settingsBtn.addEventListener('click', event => {
+	mainScreen.classList.add('hide');
+	settings.classList.remove('hide');
+})
 
+saveBtn.addEventListener('click', event => {
+	mainScreen.classList.remove('hide');
+	settings.classList.add('hide');
+})
