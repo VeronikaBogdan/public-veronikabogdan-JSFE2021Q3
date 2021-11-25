ffcc7304ad2dd70c6834fc80788406b5987e1129
Veronika <veron9399bog@gmail.com>
2021-11-25 03:26:17 +0300

    feat: add categories page

--- art-quiz/js/main.js
+++ art-quiz/js/main.js
@@ -7,10 +7,13 @@ const settingsBtn = document.querySelector('.settings-btn');
 const saveBtn = document.querySelector('.save-btn');
 
 const categories = document.querySelector('.categories');
+const homeBtn = document.querySelector('.home-btn');
+const scoreBtn = document.querySelector('.score-btn');
 
-
+//! Main Screen
 artists.addEventListener('click', event => {
 	mainScreen.classList.add('hide');
+	categories.classList.remove('hide');
 
 })
 
@@ -23,7 +26,15 @@ settingsBtn.addEventListener('click', event => {
 	settings.classList.remove('hide');
 })
 
+//! Settings
 saveBtn.addEventListener('click', event => {
 	mainScreen.classList.remove('hide');
 	settings.classList.add('hide');
 })
+
+//! Categories 
+homeBtn.addEventListener('click', event => {
+	mainScreen.classList.remove('hide');
+	categories.classList.add('hide');
+})
+
