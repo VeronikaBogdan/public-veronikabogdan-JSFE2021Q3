7664f64d9744bf3292507befdb0b6a5c70491991
Veronika <veron9399bog@gmail.com>
2021-12-07 02:14:09 +0300

    fix: add apiKey

--- migration-to-typescript/src/components/controller/appLoader.js
+++ migration-to-typescript/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd8de63ac42754b9f8a42551770559e9c', // получите свой ключ https://newsapi.org/
         });
     }
 }
