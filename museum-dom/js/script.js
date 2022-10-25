cc1c1451599445cb4f17866669dcfa70a4800771
VeronikaBogdan <bog-dan7777@mail.ru>
2022-10-26 01:13:09 +0300

    refactor: refactor the theme change

--- museum-dom/js/script.js
+++ museum-dom/js/script.js
@@ -1,75 +1,57 @@
-'use strict';
-
-console.log(	
-	`Score: 46/ 150`,
-	`
-	1. Слайдер сравнения изображений в секции Explore +10
-	2. Анимация при прокрутке изображений в секции Galery +8
-	3. Интерактивная карта в секции Contacts +12
-	4. Доп. функционал: ночная тема.. +10
-	5. Слайдер в секции Welcome: 
-			- есть возможность перелистывания слайдов кликами по буллетам (квадратики внизу слайдера) +2
-			-слайды перелистываются плавно с анимацией смещения вправо или влево +4 
-	`
-);
+"use strict";
 
 function myFunction() {
   let myTop = document.getElementById("myTopnav");
 
-	if (myTop.className === "navbar topnav") {
-		myTop.className += " responsive";
-	}
+  if (myTop.className === "navbar topnav") {
+    myTop.className += " responsive";
+  }
 }
-
-
-
-
 
 //! Welcome Slider
 
 var swiper = new Swiper(".mySwiper", {
-	spaceBetween: 5,
-	slidesPerView: 35,
-	grabCursor: true,
-	freeMode: false,
-	watchSlidesVisibility: false,
-	watchSlidesProgress: false,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	// loop: true, // бесконечная прокрутка
+  spaceBetween: 5,
+  slidesPerView: 35,
+  grabCursor: true,
+  freeMode: false,
+  watchSlidesVisibility: false,
+  watchSlidesProgress: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // loop: true, // бесконечная прокрутка
 });
 
-// верхний 
+// верхний
 var swiper2 = new Swiper(".mySwiper2", {
-	spaceBetween: 10,
-	speed: 900,
-	grabCursor: true,
-	watchSlidesVisibility: true,
-	watchSlidesProgress: true,
-	thumbs: {
-		swiper: swiper,
-	},
-	scrollbar: {
-		el: '.swiper-scrollbar',
-	},	
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+  spaceBetween: 10,
+  speed: 900,
+  grabCursor: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  thumbs: {
+    swiper: swiper,
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 const arrSlideBtn = document.slideBtn,
-			swiperSlide = document.querySelectorAll('.swiper-slide'),
-			slideBtn = document.querySelectorAll('.slide-btn'),
-			next = document.querySelector('.next');
+  swiperSlide = document.querySelectorAll(".swiper-slide"),
+  slideBtn = document.querySelectorAll(".slide-btn"),
+  next = document.querySelector(".next");
 
 let k = 0;
 
-for(let i = 0; i<slideBtn.length; i++) {
-		console.log(slideBtn[i].classList.contains('active'), k=i);
-
+for (let i = 0; i < slideBtn.length; i++) {
+  console.log(slideBtn[i].classList.contains("active"), (k = i));
 }
 
 // console.log(slideBtn);
@@ -86,45 +68,42 @@ for(let i = 0; i<slideBtn.length; i++) {
 // 	}
 // });
 
-
-
 //! Explore Slider
-const slider = document.getElementById('before-after-slider');
-const before = document.getElementById('before-image');
-const beforeImage = before.getElementsByTagName('img')[0];
-const resizer = document.getElementById('resizer');
+const slider = document.getElementById("before-after-slider");
+const before = document.getElementById("before-image");
+const beforeImage = before.getElementsByTagName("img")[0];
+const resizer = document.getElementById("resizer");
 
 let active = false;
 
 //Sort overflow out for Overlay Image
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   let width = /*slider.offsetWidth =*/ 720;
-  beforeImage.style.width = width + 'px';
+  beforeImage.style.width = width + "px";
 });
 
-//Adjust width of image on resize 
-window.addEventListener('resize', function() {
+//Adjust width of image on resize
+window.addEventListener("resize", function () {
   let width = slider.offsetWidth;
-  beforeImage.style.width = width + 'px';
-})
+  beforeImage.style.width = width + "px";
+});
 
-resizer.addEventListener('mousedown',function(){
+resizer.addEventListener("mousedown", function () {
   active = true;
- resizer.classList.add('resize');
-
+  resizer.classList.add("resize");
 });
 
-document.body.addEventListener('mouseup',function(){
+document.body.addEventListener("mouseup", function () {
   active = false;
- resizer.classList.remove('resize');
+  resizer.classList.remove("resize");
 });
 
-document.body.addEventListener('mouseleave', function() {
+document.body.addEventListener("mouseleave", function () {
   active = false;
-  resizer.classList.remove('resize');
+  resizer.classList.remove("resize");
 });
 
-document.body.addEventListener('mousemove',function(e){
+document.body.addEventListener("mousemove", function (e) {
   if (!active) return;
   let x = e.pageX;
   x -= slider.getBoundingClientRect().left;
@@ -132,173 +111,159 @@ document.body.addEventListener('mousemove',function(e){
   pauseEvent(e);
 });
 
-resizer.addEventListener('touchstart',function(){
+resizer.addEventListener("touchstart", function () {
   active = true;
-  resizer.classList.add('resize');
+  resizer.classList.add("resize");
 });
 
-document.body.addEventListener('touchend',function(){
+document.body.addEventListener("touchend", function () {
   active = false;
-  resizer.classList.remove('resize');
+  resizer.classList.remove("resize");
 });
 
-document.body.addEventListener('touchcancel',function(){
+document.body.addEventListener("touchcancel", function () {
   active = false;
-  resizer.classList.remove('resize');
+  resizer.classList.remove("resize");
 });
 
 //calculation for dragging on touch devices
-document.body.addEventListener('touchmove',function(e){
+document.body.addEventListener("touchmove", function (e) {
   if (!active) return;
   let x;
-  
+
   let i;
-  for (i=0; i < e.changedTouches.length; i++) {
-  x = e.changedTouches[i].pageX; 
+  for (i = 0; i < e.changedTouches.length; i++) {
+    x = e.changedTouches[i].pageX;
   }
-  
+
   x -= slider.getBoundingClientRect().left;
   slideIt(x);
   pauseEvent(e);
 });
 
-function slideIt(x){
-    let transform = Math.max(0,(Math.min(x,slider.offsetWidth)));
-    before.style.width = transform+"px";
-    resizer.style.left = transform-0+"px";
+function slideIt(x) {
+  let transform = Math.max(0, Math.min(x, slider.offsetWidth));
+  before.style.width = transform + "px";
+  resizer.style.left = transform - 0 + "px";
 }
 
 //stop divs being selected.
-function pauseEvent(e){
-    if(e.stopPropagation) e.stopPropagation();
-    if(e.preventDefault) e.preventDefault();
-    e.cancelBubble=true;
-    e.returnValue=false;
-    return false;
+function pauseEvent(e) {
+  if (e.stopPropagation) e.stopPropagation();
+  if (e.preventDefault) e.preventDefault();
+  e.cancelBubble = true;
+  e.returnValue = false;
+  return false;
 }
-
-
-
 
 //! Gallery
 
-const pictureInnerContainer = document.querySelector('.picture-inner-container');
+const pictureInnerContainer = document.querySelector(
+  ".picture-inner-container"
+);
 
 AOS.init({
-	delay: 300,
-	duration: 1000,
-	targetSelector: '.picture-inner-container',
+  delay: 300,
+  duration: 1000,
+  targetSelector: ".picture-inner-container",
 });
 
 for (let i = 0; i < 15; i++) {
-	let img = document.createElement('img');
-	img.classList.add('.picture-img');
+  let img = document.createElement("img");
+  img.classList.add(".picture-img");
 
-	img.setAttribute('data-aos', 'fade-up');
-	img.setAttribute('data-aos-anchor-placement', 'top-bottom');
+  img.setAttribute("data-aos", "fade-up");
+  img.setAttribute("data-aos-anchor-placement", "top-bottom");
 
-	img.setAttribute('alt', 'photo: pictures and sculptures');
-	img.src = `assets/img/galery/galery${i+1}.jpg`;  
-	pictureInnerContainer.append(img);
+  img.setAttribute("alt", "photo: pictures and sculptures");
+  img.src = `assets/img/galery/galery${i + 1}.jpg`;
+  pictureInnerContainer.append(img);
 }
-
 
 let scrollRef = 0;
 
-window.addEventListener('scroll', function () {
-	//aus bug solution
-	scrollRef <= 10 ? scrollRef++ : AOS.refresh();
+window.addEventListener("scroll", function () {
+  //aus bug solution
+  scrollRef <= 10 ? scrollRef++ : AOS.refresh();
 });
-
-
-
 
 //! Map in Contacts
 
 (function createMap() {
-const apiKey = 'pk.eyJ1IjoiYWxmcmVkMjAxNiIsImEiOiJja2RoMHkyd2wwdnZjMnJ0MTJwbnVmeng5In0.E4QbAFjiWLY8k3AFhDtErA';
+  const apiKey =
+    "pk.eyJ1IjoiYWxmcmVkMjAxNiIsImEiOiJja2RoMHkyd2wwdnZjMnJ0MTJwbnVmeng5In0.E4QbAFjiWLY8k3AFhDtErA";
 
-const myMap = L.map('map').setView([48.86091, 2.3364], 17);
+  const myMap = L.map("map").setView([48.86091, 2.3364], 17);
 
-L.tileLayer(
-	'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-	{
-		maxZoom: 18,
-		id: 'mapbox/light-v10',
-		tileSize: 512,
-		zoomOffset: -1,
-		accessToken: apiKey,
-	},
-).addTo(myMap);
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      maxZoom: 18,
+      id: "mapbox/light-v10",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: apiKey,
+    }
+  ).addTo(myMap);
 
-var myIcon = L.icon({
-	iconUrl: 'assets/svg/place_black_24dp.svg',
-	iconSize: [35, 35],
-});
+  var myIcon = L.icon({
+    iconUrl: "assets/svg/place_black_24dp.svg",
+    iconSize: [35, 35],
+  });
 
-const mainMarker = L.marker([48.86091, 2.3364], { icon: myIcon }).addTo(myMap);
-document.querySelectorAll('.leaflet-marker-icon').forEach(i => (i.style.opacity = 0.6));
+  const mainMarker = L.marker([48.86091, 2.3364], { icon: myIcon }).addTo(
+    myMap
+  );
+  document
+    .querySelectorAll(".leaflet-marker-icon")
+    .forEach((i) => (i.style.opacity = 0.6));
 
-// Adding Marker
-let mapPopup = [
-	[48.8602, 2.3333],
-	[48.8607, 2.3397],
-	[48.8619, 2.333],
-	[48.8625, 2.3365]
-];
-mapPopup.forEach(point => {
-	const marker = L.marker(point, { icon: myIcon }).addTo(myMap);
-});
-document.querySelectorAll('.leaflet-marker-icon').forEach(i => (i == 0 ? i.style.opacity = 1 : i.style.opacity = 0.6));
-
+  // Adding Marker
+  let mapPopup = [
+    [48.8602, 2.3333],
+    [48.8607, 2.3397],
+    [48.8619, 2.333],
+    [48.8625, 2.3365],
+  ];
+  mapPopup.forEach((point) => {
+    const marker = L.marker(point, { icon: myIcon }).addTo(myMap);
+  });
+  document
+    .querySelectorAll(".leaflet-marker-icon")
+    .forEach((i) => (i == 0 ? (i.style.opacity = 1) : (i.style.opacity = 0.6)));
 })();
-
 
 //! Theme
 
-const buttonTheme = document.querySelector('.button-theme'),
-		 	buttonThemeCl = document.querySelector('.button-theme-cl'),
-			themeIcon = document.querySelector('.icon-theme-open'),
-			arrImages = document.images;
+const buttonTheme = document.querySelector(".button-theme");
+const buttonThemeCl = document.querySelector(".button-theme-cl");
+const themeIcon = document.querySelector(".icon-theme-open");
+const sections = document.querySelectorAll(".section");
+const arrImages = document.images;
 
 buttonTheme.addEventListener("click", () => {
-	themeIcon.setAttribute('src','assets/svg/dark_mode_black_24dp.svg'); //меняем цвет иконки, устанавливая в src нужную иконку
-	buttonTheme.classList.toggle('button-theme-cl');
-	buttonThemeCl.classList.toggle('button-theme');
-	buttonThemeCl.classList.remove('button-theme-cl');
+  buttonTheme.classList.toggle("button-theme-cl");
+  buttonThemeCl.classList.toggle("button-theme");
+  buttonThemeCl.classList.remove("button-theme-cl");
 
-	document.body.style.cssText = `background: #000; color: #fff;`;
-	document.documentElement.style.setProperty('--font-black', '#fff');
-	document.documentElement.style.setProperty('--font-light', '#000');
-	document.documentElement.style.setProperty('--bg-dark', '#eee');
-	document.documentElement.style.setProperty('--font-gold', '#d3943c');
-	document.documentElement.style.setProperty('--dark-red', '#710707');
-
-	for(let key of arrImages) {
-		if(key.classList.contains('logo')){
-			key.setAttribute('src','assets/svg/logoBlack.svg');
-		}
-	}
-
+  sections.forEach((item) => {
+    item.classList.add("dark-section-cl");
+    document.documentElement.style.setProperty("--font-black", "#fff");
+    if (item.classList.contains("dark-section-cl")) {
+      document.documentElement.style.setProperty("--font-black", "#fff");
+      document.documentElement.style.setProperty("--font-gold", "#d3943c");
+    }
+  });
 });
 
 buttonThemeCl.addEventListener("click", () => {
-	themeIcon.setAttribute('src','assets/svg/dark_mode_white_24dp.svg'); //меняем цвет иконки, устанавливая в src нужную иконку
-	buttonTheme.classList.toggle('button-theme-cl');
-	buttonThemeCl.classList.remove('button-theme');
-	buttonThemeCl.classList.toggle('button-theme-cl');
+  buttonTheme.classList.toggle("button-theme-cl");
+  buttonThemeCl.classList.remove("button-theme");
+  buttonThemeCl.classList.toggle("button-theme-cl");
 
-	document.body.style.cssText = `background: #fff; color: #000;`;
-	document.documentElement.style.setProperty('--font-black', '#000');
-	document.documentElement.style.setProperty('--font-light', '#fff');
-	document.documentElement.style.setProperty('--bg-dark', '#030303');
-	document.documentElement.style.setProperty('--font-gold', '#9d8665');
-	document.documentElement.style.setProperty('--dark-red', '#710707');
-
-	for(let key of arrImages) {
-		if(key.classList.contains('logo')){
-			key.setAttribute('src','assets/svg/logo.svg');
-		}
-	}
-
+  sections.forEach((item) => {
+    item.classList.remove("dark-section-cl");
+  });
+  document.documentElement.style.setProperty("--font-black", "#000");
+  document.documentElement.style.setProperty("--font-gold", "#9d8665");
 });
